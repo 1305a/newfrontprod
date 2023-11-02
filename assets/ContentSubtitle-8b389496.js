0ee777096af84287fd52b5c954c5c64dcfb772b2
Andrey <1305andrew2@gmail.com>
2023-11-02 14:35:35 +0300

    change path

--- assets/ContentSubtitle-8b389496.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c as o}from"./index-867c0113.js";const s=["innerHTML"],l={__name:"ContentSubtitle",props:["componentData"],setup(t){const e=t;return(c,_)=>(n(),o("div",{innerHTML:e.componentData.subtitle,class:"content-subtitle"},null,8,s))}};export{l as default};
